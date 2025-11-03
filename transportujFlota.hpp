c79d3ab829613fad26565eea9ea89d1757ffa88a
jjanu <jjanu@YUNGJANUARY60>
2025-11-03 17:16:39 +0100

    uzupelniona funkcja transportujflota

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+        if (towar == 0)
+            return 0;
+
+        Stocznia     stocznia;         
+        unsigned int suma         = 0; 
+        unsigned int ileZaglowcow = 0;
+
+        while (suma < towar) {
+            
+            Statek* s = stocznia(); 
+            unsigned int ile = s->transportuj();
+            suma += ile;
+            if (dynamic_cast< Zaglowiec* >(s) != nullptr)
+                ++ileZaglowcow;   
+            delete s;
+        }
+        return ileZaglowcow;
+ 
     return 0;
 }
